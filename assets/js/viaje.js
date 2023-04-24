f278de030ecf3eac0f136be4215d0c7b6c7db9e5
Andrés Swartz <kalirider377@gmail.com>
2023-04-24 19:33:27 -0400

    Primer commit

---
+++ assets/js/viaje.js
@@ -0,0 +1,31 @@
+$( "#desar1" ).click(function() {
+    $( "h6" ).toggle();
+  });
+
+  $( "#desar2" ).click(function() {
+    $( "h6" ).toggle();
+  });
+
+  $( "#desar3" ).click(function() {
+    $( "h6" ).toggle();
+  });
+
+  $( "#desar4" ).click(function() {
+    $( "h6" ).toggle();
+  });
+
+
+  function envia() {
+    alert("El mensaje fue enviado correctamente");
+  }
+
+
+  $(document).ready(function(){
+    $("#hdsw").click(function(){
+      $("p").toggle();
+    });
+  });
+
+
+
+  
